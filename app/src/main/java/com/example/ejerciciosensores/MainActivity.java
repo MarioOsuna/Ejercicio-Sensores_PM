758c4f5c1e44307e8375da255083dc01c3c87866
MarioOsuna <marioosuna26@gmail.com>
2019-11-23 12:36:57 +0100

    Todo correcto

--- app/src/main/java/com/example/ejerciciosensores/MainActivity.java
+++ app/src/main/java/com/example/ejerciciosensores/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejerciciosensores;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -10,6 +11,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
@@ -18,8 +21,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     SensorManager sensorManager;
-    Sensor gyroscopeSensor, rotationVectorSensor;
-    TextView textView1, textView2;
+
     GridLayout gridLayout;
     MediaPlayer mediaPlayer;
 
@@ -29,24 +31,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         gridLayout = findViewById(R.id.gridLayout);
-        textView1 = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
+
+
         mediaPlayer = MediaPlayer.create(this, R.raw.qotsa);
-
-
         List<Sensor> lista = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), sensorManager.SENSOR_DELAY_NORMAL);
-        // sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), sensorManager.SENSOR_DELAY_NORMAL);
 
-        //sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        // Register the listener
-        sensorManager.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), sensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), sensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        boolean SAGIRAO=false;
+
         if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             if (event.values[0] == 0) {
                 Random rand = new Random();
@@ -59,26 +56,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
 
         }
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            textView1.setText(" "+ event.values[2]);
-            if (event.values[2] > 0.5f) { // anticlockwise
-                SAGIRAO=true;
-                mediaPlayer.start();
-                textView2.setText(" "+ event.values[2]);
-                gridLayout.setBackgroundColor(Color.BLUE);
-            }
-            else if(mediaPlayer.isPlaying()){
-                mediaPlayer.pause();
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
 
-                /* else if (event.values[2] < -0.5f) { // clockwise
-                SAGIRAO=true;
+            int rotation = display.getRotation();
+
+            // El objeto devuelve 3 estados 0,1 y 3
+            if (rotation == 0) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+
+                }
+
+
+            } else if (mediaPlayer.isPlaying()) {
+
+            } else {
                 mediaPlayer.start();
-                gridLayout.setBackgroundColor(Color.YELLOW);
-                textView2.setText(" "+ event.values[2]);
-            }*/
+
             }
-           /* if(SAGIRAO)
-            mediaPlayer.pause();*/
+
         }
 
 
